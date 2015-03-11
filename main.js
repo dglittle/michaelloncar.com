8399d434b6f6b75df95c83c52e07eeece1d9c3b3
Josh Lankford <lankford.josh@gmail.com>
2015-03-11 02:01:59 -0700

    beep

---
+++ main.js
@@ -0,0 +1,20 @@
+var strVar="";
+strVar += " <div class=\"footer\">";
+strVar += "   <div class=\"follow\"> <span class=\"fol_txt\">Follow";
+strVar += "     MichaelLoncar.com     on:<\/span> <span><a href=\"http:\/\/twitter.com\/\"><img src=\".\/Home_files\/f1.png\" alt=\"Twitter\"><\/a><\/span> <span><a href=\"http:\/\/michaelloncar.com\/#\"><img src=\".\/Home_files\/f2.png\" alt=\"Twitter\"><\/a><\/span> <span><a href=\"http:\/\/facebook.com\/\"><img src=\".\/Home_files\/f3.png\" alt=\"Twitter\"><\/a><\/span> <span><a href=\"http:\/\/michaelloncar.com\/#\"><img src=\".\/Home_files\/f4.png\" alt=\"Twitter\"><\/a><\/span> <span><a href=\"http:\/\/michaelloncar.com\/#\"><img src=\".\/Home_files\/f5.png\" alt=\"Twitter\"><\/a><\/span> <span><a href=\"http:\/\/michaelloncar.com\/#\"><img src=\".\/Home_files\/f6.png\" alt=\"Twitter\"><\/a><\/span> <\/div>";
+strVar += "   <div class=\"foot_menu\">";
+strVar += "     <ul id=\"menu-menu-1\" class=\"menu\"><li class=\"menu-item menu-item-type-post_type current-menu-item page_item page-item-6 current_page_item menu-item-14\" style=\"padding-left: 0px;\"><a href=\"Home.html\"><span class=\"slash\" style=\"display: none;\">\/<\/span><span class=\"slash_hov\">Home<\/span><\/a><\/li>";
+strVar += "<li class=\"menu-item menu-item-type-post_type menu-item-15\"><a href=\"AboutMe.html\"><span class=\"slash\">\/<\/span><span class=\"slash_hov\">About Me<\/span><\/a><\/li>";
+strVar += "<li class=\"menu-item menu-item-type-post_type menu-item-466\"><a href=\"Blog.html\"><span class=\"slash\">\/<\/span><span class=\"slash_hov\">Blog<\/span><\/a><\/li>";
+strVar += "<li class=\"menu-item menu-item-type-post_type menu-item-766\"><a href=\"PokerUpFront.html\"><span class=\"slash\">\/<\/span><span class=\"slash_hov\">PokerUpFront<\/span><\/a><\/li>";
+strVar += "<li class=\"menu-item menu-item-type-post_type menu-item-244\"><a href=\"Gallery.html\"><span class=\"slash\">\/<\/span><span class=\"slash_hov\">Gallery<\/span><\/a><\/li>";
+strVar += "<\/ul>     <div class=\"clear\"><\/div>";
+strVar += "   <\/div>";
+strVar += "   <div class=\"copy\"><a href=\".\/Home_files\/Home.html\" title=\"MichaelLoncar.com\">";
+strVar += "     MichaelLoncar.com     <\/a> ©";
+strVar += "     2015      \/ <a href=\"http:\/\/michaelloncar.com\/?page_id=285\">Priacy Policy<\/a><\/div>";
+strVar += " <\/div>";
+
+$(function () {
+  $('.foot').append(strVar);
+});
